42c3c5e58734b80894343a1b402d18ec84d0d19b
pumpkinluka <gabriella.beltran@ucas-edu.net>
2026-09-10 14:20:49 -0600

    Variables Practice

--- variables_practice.py
+++ variables_practice.py
@@ -1,1 +1,14 @@
 # GB, Variables Practice
+
+name = "Remilia"
+number = "7"
+big_number = "293"
+breakfast_food = "pancakes"
+fav_color = "pink"
+school = "UCAS"
+year = "2026"
+eye_color = "brown"
+age = "14"
+fav_subject = "Biology"
+
+print(name, number, big_number, breakfast_food, fav_color, school, year, eye_color, age, fav_subject)
