2c6ef089618bb3eb2daec6fd5fef5784175c196c
pumpkinluka <gabriella.beltran@ucas-edu.net>
2026-09-14 14:50:47 -0600

    Memory Diagram Activity

---
+++ memory_diagram.py
@@ -0,0 +1,23 @@
+# GB, Memory Diagram Activity
+
+name = "Marisa"
+print(f"The variable name has a value of {name} and is saved at {id(name)}")
+age = 14
+print(f"The variable age has a value of {age} and is saved at {id(name)}")
+
+score = 10
+print(f"The variable score has a value of {score} and is saved at {id(score)}")
+score = 25
+print(f"The variable score has a value of {score} and is saved at {id(score)}")
+
+height = 5.8
+print(f"The variable height has a value of {height} and is saved at {id(height)}")
+city = "Las Vegas"
+print(f"The variable city has a value of {city} and is saved at {id(city)}")
+zip_code = 89101
+print(f"The variable zip_code has a value of {zip_code} and is saved at {id(zip_code)}")
+
+price1 = 10
+print(f"The variable price1 has a value of {price1} and is saved at {id(price1)}")
+price2 = 10
+print(f"The variable price2 has a value of {price2} and is saved at {id(price2)}")
