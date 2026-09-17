d7113ca12e551186262e1b65003ea934add35ff2
pumpkinluka <gabriella.beltran@ucas-edu.net>
2026-09-17 23:43:39 +0000

    your budget

--- your_budget.py
+++ your_budget.py
@@ -38,12 +38,9 @@ while True:
 print(f"Your rent is ${rent:.2f} and that is {int(rent/income*100)}% of your income.")
 print(f"Your utilities is ${util:.2f} and that is {int(util/income*100)}% of your income.")
 print(f"Your groceries is ${groceries:.2f} and that is {int(groceries/income*100)}% of your income.")
-print(f"Your transportation is ${transport:.2f} and that is {int(transport/income*100)}%")
+print(f"Your transportation is ${transport:.2f} and that is {int(transport/income*100)}% of your income.")
 
-
-print(f"You should save ${income/10} and that is 10% of your income")
-print(f"You have  of spending money each month ")
-
-# rent/income*100
-
-# f"${rent:.2f}"
+savings = income/10
+leftover = income-(rent+util+groceries+transport
+print(f"You should save ${income/10} a month, that is 10% of your income.")
+print(f"You have ${leftover:money each month!")
