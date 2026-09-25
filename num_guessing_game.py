1c1bb6e9606480ebccfe7235068ab332682f230a
pumpkinluka <gabriella.beltran@ucas-edu.net>
2026-09-25 23:46:12 +0000

    Number Guessing Game

--- num_guessing_game.py
+++ num_guessing_game.py
@@ -1,26 +1,31 @@
 # GB. 7th. Number Guessing Game
 
+# Secret number is between 1-100
+# The player gets 7 attempts
+
 import random
 
-print("Ready to play? I'm thinking of a number between 1 and 100. You have 5 tries to guess it!")
+print("Ready to play? I'm thinking of a number between 1 and 100. You have 7 tries to guess it!")
 
-number = random.randint(1,101)
-guess = input("Guess #1: ")
-tries = 0
-low = print("Too low!")
-high = print("Too high!")
+number = random.randint(1,100)
+max = 7
+guess_num = 1
 
-while True:
-    if number == guess:
+for tries in range(max):
+    guess = int(input(f"Guess #{guess_num}: "))
+
+    if number < guess:
+        tries += 1
+        guess_num += 1
+        print("Too high!")
+    elif guess < number:
+        tries += 1
+        guess_num += 1
+        print("Too low!")
+    else: 
         tries += 1
         print(f"Correct! You guessed it in {tries} tries!")
         break
-    elif guess > number:
-        tries += 1
-        print(high)
-    elif guess < number:
-        tries += 1
-        print(low)
 
-if tries == 5:
+if number != guess and tries == 7:
     print(f"Too bad... you're out of guesses! The number was {number}.")
